0c28473fd4b66f7b009907c658c0537c526e5378
Shoffly <80728185+Shoffly@users.noreply.github.com>
2024-04-30 18:19:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ def main():
 
     if uploaded_file is not None:
         # Read CSV
-        df = pd.read_csv(uploaded_file)
+        df = pd.read_csv(uploaded_file, encoding='utf-8-sig')
 
         # Choose column with reviews
         review_column = st.selectbox("Select review column", df.columns)
